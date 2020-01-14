211dbb1f84e01b91262684710f4825249edb9354
Gaveux <shua9815@uni.sydney.edu.au>
2020-01-14 22:49:00 +1100

    calculating the moving average of bead coordinates for each atom     over the number of MC-iteration. moving-averaged bead coordinates     are printed out in a separated file per atom

--- coord_convert.F90
+++ coord_convert.F90
@@ -7,27 +7,41 @@
 
      implicit none
 
-     type(pimc_particle),dimension(:), pointer :: Beads
+     type(pimc_particle),dimension(:), pointer :: Beads, BeadSum
      
   
      integer :: i,j,k,line,ierr,skip, lines
      integer :: move, natom, dimen, NumBeadsEff
      real(kind=8), dimension(3) :: y    
-     character(len=100) :: filename
+     character(len=100) :: filename, atom1, atom2, atom3, atom4, atom5
      character(len=3) :: arg2
      integer :: BeadNumber
+     integer :: a1,a2,a3,a4,a5
 
      call getarg(1,filename)
      call getarg(2,arg2)
+     call getarg(3,atom1)
+     call getarg(4,atom2)
+     call getarg(5,atom3)
+     call getarg(6,atom4)
+     call getarg(7,atom5)
      read (arg2,*) BeadNumber
      allocate(Beads(BeadNumber),stat=ierr)
      if (ierr.ne.0) stop 'Error allocating Beads array'
+     allocate(BeadSum(BeadNumber),stat=ierr)
+     if (ierr.ne.0) stop 'Error allocating BeadSum array'
 
      do i=1,size(Beads)
         call new(Beads(i),5,3)
+        call new(BeadSum(i),5,3)
      enddo
     
      open(unit=7,file=trim(filename),status='unknown',action='read',position='rewind')
+     open(newunit=a1,file=trim(atom1),status='unknown',action='write')
+     open(newunit=a2,file=trim(atom2),status='unknown',action='write')
+     open(newunit=a3,file=trim(atom3),status='unknown',action='write')
+     open(newunit=a4,file=trim(atom4),status='unknown',action='write')
+     open(newunit=a5,file=trim(atom5),status='unknown',action='write')
        do move = 1, 1000000
 
          do NumBeadsEff = 1,BeadNumber
@@ -39,16 +53,29 @@
          ! do transformation operations here
          call coord_translation(Beads,BeadNumber)
          call coord_rotation(Beads,BeadNumber)
-        do NumBeadsEff = 1,BeadNumber
+        !do NumBeadsEff = 1,BeadNumber
+        !   do natom = 1,5
+        !      print *, Beads(NumBeadsEff)%x(:,natom)
+        !   enddo
+        !enddo 
+
+        ! do moving average of Beads coordinates
+        do NumBeadsEff =1,BeadNumber
            do natom = 1,5
-              print *, Beads(NumBeadsEff)%x(:,natom)
+             BeadSum(NumBeadsEff)%x(:,natom) = BeadSum(NumBeadsEff)%x(:,natom) + Beads(NumBeadsEff)%x(:,natom) 
            enddo
-        enddo 
+           write(a1,*) BeadSum(NumBeadsEff)%x(:,1)/dble(move)
+           write(a2,*) BeadSum(NumBeadsEff)%x(:,2)/dble(move)
+           write(a3,*) BeadSum(NumBeadsEff)%x(:,3)/dble(move)
+           write(a4,*) BeadSum(NumBeadsEff)%x(:,4)/dble(move)
+           write(a5,*) BeadSum(NumBeadsEff)%x(:,5)/dble(move)
+        enddo
        enddo
 
      
      close(unit=7)
      deallocate(Beads)
+     deallocate(BeadSum)
 
    end program
 
