bb9dd71b2a3d8e9672bc8df6627b3644710e2090
Gaveuxifjørt <shua9815@uni.sydney.edu.au>
2020-02-26 10:24:56 +1100

    adding ca_cv subroutine in estimator_class.F90

--- estimator_class.F90
+++ estimator_class.F90
@@ -96,6 +96,11 @@ module Estimator_class
                     call update(this%vars(1),results(1))
                     call update(this%vars(4),results(2))
                     call update(this%vars(7),results(3))
+  
+                    call energy_ca_cv(sys,pimc,Beads,results)
+                    call update(this%vars(3),results(1))
+                    call update(this%vars(6),results(2))
+                    call update(this%vars(9),results(3))
 
                 !Else if we are using the suzuki-chin action use the suzuki-chin action estimators
                 else if (pimc%act%act_type.eq.3) then
@@ -445,6 +450,7 @@ module Estimator_class
         !Estimators for the chin action
         !==========================================================================
 
+
         subroutine energy_thermo_ca(sys,pimc,Beads,results)
             type (molsysdat), intent(in) :: sys
             type (pimc_par), intent(in) :: pimc
@@ -524,6 +530,99 @@ module Estimator_class
             return
         end subroutine energy_thermo_ca
 
+     
+        subroutine energy_ca_cv(sys,pimc,Beads,results)
+            type (molsysdat), intent(in) :: sys
+            type (pimc_par), intent(in) :: pimc
+            type (pimc_particle), dimension(:), pointer :: Beads
+            real(kind=8), dimension(3), intent(out) :: results
+            real(kind=8), dimension(sys%dimen,sys%natom) :: cv
+
+            
+            real(kind=8) :: e, ke, pe, frc, dsq, tdsq, ttemp_frc, temp_frc
+            real(kind=8) :: dsq_a, dsq_b, dsq_c, frc_a, frc_b, frc_c, pe_a, pe_b, pe_c
+            real(kind=8) :: n2b
+
+            integer :: i,j,k
+
+            ke = 0.0
+            pe = 0.0
+            frc = 0.0
+            dsq_a=0.0
+            dsq_b=0.0
+            dsq_c=0.0
+            frc_a=0.0
+            frc_b=0.0
+            frc_c=0.0
+            pe_a=0.0
+            pe_b=0.0
+            pe_c=0.0
+            
+            ! compute the centroid
+            do i=1,sys%natom
+                do j=1,sys%dimen
+                    cv(j,i)=0.0
+                    do k=1,pimc%NumBeads
+                        cv(j,i)=cv(j,i)+Beads(k)%x(j,i)
+                    enddo
+                    cv(j,i)=cv(j,i)*pimc%invNumBeads
+                enddo
+            enddo
+
+  
+            do i=0,pimc%NumBeads-1    
+                dsq = 0.0
+                temp_frc=0.0
+                
+                do j=1,sys%natom
+                    tdsq=0.0
+                    ttemp_frc=0.0
+                    do k=1,sys%dimen
+                        !index as given because fortran is one indexed
+                        dsq_a = pimc%act%t1inv*(Beads(3*i+1)%x(k,j)-Beads(3*i+2)%x(k,j))**2
+                        dsq_b = pimc%act%t1inv*(Beads(3*i+2)%x(k,j)-Beads(3*i+3)%x(k,j))**2 
+                        dsq_c = 0.5*pimc%act%t0inv*(Beads(3*i+3)%x(k,j)-Beads(3*i+4)%x(k,j))**2 
+                    
+                        tdsq = tdsq + (dsq_a+dsq_b+dsq_c)
+                    
+                        frc_a = pimc%act%a1*Beads(3*i+1)%dVdx(k,j)**2
+                        frc_b= (1.0-2.0*pimc%act%a1)*Beads(3*i+2)%dVdx(k,j)**2
+                        frc_c= pimc%act%a1*Beads(3*i+3)%dVdx(k,j)**2
+                        
+                        ttemp_frc = ttemp_frc + (frc_a+frc_b+frc_c)
+
+                    enddo
+                    dsq = dsq + tdsq*sys%mass(j)
+                    temp_frc = temp_frc + ttemp_frc/sys%mass(j)
+                enddo
+
+                ke = ke + dsq
+                frc = frc + temp_frc
+                pe_a=Beads(3*i+1)%VCurr*pimc%act%v1
+                pe_b=Beads(3*i+2)%VCurr*pimc%act%v2
+                pe_c=Beads(3*i+3)%VCurr*pimc%act%v1
+                pe = pe + pe_a + pe_b + pe_c
+
+            enddo
+
+            pe = pe*pimc%invNumBeads
+            ke = ke*pimc%NumBeads * (0.5*pimc%invBeta**2)
+            frc = frc*pimc%act%u0*pimc%Beta**2*(pimc%invNumBeads**3)
+
+            !n2b = 3NDP/2beta for this case we have hardcoded 
+            n2b = 1.5*dble(sys%dimen)*dble(sys%natom)*dble(pimc%NumBeads)*pimc%invBeta
+
+            ke = n2b - ke + frc
+            pe = pe + 2.0*frc
+            e = ke + pe 
+ 
+            !Update the energies
+            results(1)=e
+            results(2)=ke
+            results(3)=pe
+            
+            return
+        end subroutine energy_ca_cv
 
         !==========================================================================
         !==========================================================================
